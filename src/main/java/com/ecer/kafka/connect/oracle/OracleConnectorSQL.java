f626e44e234aa993b958e301101cac86b03ba9c3
ggarreton <german.garreton@gmail.com>
2019-12-23 15:42:28 -0300

    missing semicolons

--- src/main/java/com/ecer/kafka/connect/oracle/OracleConnectorSQL.java
+++ src/main/java/com/ecer/kafka/connect/oracle/OracleConnectorSQL.java
@@ -1,9 +1,9 @@
 package com.ecer.kafka.connect.oracle;
 
 public interface OracleConnectorSQL{
-    String SET_LOGFILE9i="begin \nDBMS_LOGMNR.ADD_LOGFILE(LOGFILENAME => '/u01/app/oracle/oradata/odbms/redo01.log'); \nend;"
-    String SET_LOGFILE9i="begin \nDBMS_LOGMNR.ADD_LOGFILE(LOGFILENAME => '/u01/app/oracle/oradata/odbms/redo02.log'); \nend;"
-    String SET_LOGFILE9i="begin \nDBMS_LOGMNR.ADD_LOGFILE(LOGFILENAME => '/u01/app/oracle/oradata/odbms/redo03.log'); \nend;"
+    String SET_LOGFILE9i="begin \nDBMS_LOGMNR.ADD_LOGFILE(LOGFILENAME => '/u01/app/oracle/oradata/odbms/redo01.log'); \nend;";
+    String SET_LOGFILE9i="begin \nDBMS_LOGMNR.ADD_LOGFILE(LOGFILENAME => '/u01/app/oracle/oradata/odbms/redo02.log'); \nend;";
+    String SET_LOGFILE9i="begin \nDBMS_LOGMNR.ADD_LOGFILE(LOGFILENAME => '/u01/app/oracle/oradata/odbms/redo03.log'); \nend;";
     String LOGMINER_SELECT_WITHSCHEMA="SELECT thread#, scn, start_scn, commit_scn,timestamp, operation_code, operation,status, SEG_TYPE_NAME ,info,seg_owner, table_name, username, sql_redo ,row_id, csf, TABLE_SPACE, SESSION_INFO, RS_ID, RBASQN, RBABLK, SEQUENCE#, TX_NAME, SEG_NAME, SEG_TYPE_NAME FROM  v$logmnr_contents  WHERE OPERATION_CODE in (1,2,3) and commit_scn>=? and ";
     String START_LOGMINER_CMD="begin \nDBMS_LOGMNR.START_LOGMNR(STARTSCN => ?,";
     String LOGMINER_START_OPTIONS="OPTIONS =>  DBMS_LOGMNR.SKIP_CORRUPTION+DBMS_LOGMNR.NO_SQL_DELIMITER+DBMS_LOGMNR.DICT_FROM_ONLINE_CATALOG + DBMS_LOGMNR.CONTINUOUS_MINE+DBMS_LOGMNR.COMMITTED_DATA_ONLY";
